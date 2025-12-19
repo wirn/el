3e31fffa7ff9741496830e5cb1519408716f90a6
Eric Björklund <eric.bjorklund@xlent.se>
2025-12-19 16:10:28 +0100

    upgraded to 15 min

--- src/app/pages/graph/graph/graph.component.ts
+++ src/app/pages/graph/graph/graph.component.ts
@@ -1,34 +1,29 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
-import { PriceInterval } from '../../../models/priceInterval.model';
 import { CommonModule } from '@angular/common';
+import { Component, Input, SimpleChanges } from '@angular/core';
 import { Color, NgxChartsModule, ScaleType } from '@swimlane/ngx-charts';
+import { PriceInterval } from '../../../models/priceInterval.model';
 
-// declare enum ScaleType {
-//   Time = 'time',
-//   Linear = 'linear',
-//   Ordinal = 'ordinal',
-//   Quantile = 'quantile',
-// }
-
-interface graphData {
+interface GraphData {
   name: string;
   value: number;
 }
 
 @Component({
   selector: 'app-interval-graph',
-  imports: [CommonModule, NgxChartsModule], //NgxChartsModule
+  imports: [CommonModule, NgxChartsModule],
   templateUrl: './graph.component.html',
   styleUrl: './graph.component.scss',
 })
 export class IntervalGraphComponent {
   @Input() priceIntervalList: PriceInterval[] = [];
-  mapedGraphData: graphData[] | null = null;
+
+  mappedGraphData: GraphData[] = [];
 
   view: [number, number] = [364, 615];
 
   showLabels = true;
   isDoughnut = false;
+
   colorScheme: Color = {
     name: 'custom',
     selectable: true,
@@ -37,21 +32,38 @@ export class IntervalGraphComponent {
   };
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['priceIntervalList'] && this.priceIntervalList) {
-      this.mapedGraphData = this.mapPriceIntervalsToGraphData(
-        this.priceIntervalList
+    if (changes['priceIntervalList']) {
+      this.mappedGraphData = this.mapPriceIntervalsToGraphData(
+        this.priceIntervalList ?? []
       );
     }
   }
 
-  mapPriceIntervalsToGraphData(priceIntervals: PriceInterval[]): graphData[] {
-    return priceIntervals.map((interval) => ({
-      // name:
-      //   new Date(interval.time_start).getHours().toString() +
-      //   ' - ' +
-      //   (new Date(interval.time_start).getHours() + 1).toString(),
-      name: new Date(interval.time_start).getHours().toString(),
-      value: interval.SEK_per_kWh,
-    }));
+  private mapPriceIntervalsToGraphData(
+    priceIntervals: PriceInterval[]
+  ): GraphData[] {
+    return [...priceIntervals]
+      .sort((a, b) => this.toEpoch(a.time_start) - this.toEpoch(b.time_start))
+      .map((interval) => {
+        const date = new Date(interval.time_start);
+        return {
+          name: this.formatHHmm(date),
+          value: interval.SEK_per_kWh,
+        };
+      });
+  }
+
+  private formatHHmm(date: Date): string {
+    const hh = String(date.getHours()).padStart(2, '0');
+    const mm = String(date.getMinutes()).padStart(2, '0');
+    return `${hh}:${mm}`;
+  }
+
+  private toEpoch(value: unknown): number {
+    const t =
+      value instanceof Date
+        ? value.getTime()
+        : new Date(value as any).getTime();
+    return Number.isFinite(t) ? t : 0;
   }
 }
